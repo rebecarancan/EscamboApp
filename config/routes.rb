caad7507ed33cbfc4e916d16e64344326ba95c9d
Rebeca Rancan <rebecarancanrr@gmail.com>
2018-09-06 09:43:41 -0300

    Layouts, atualizações/cadastros de categorias

---
+++ config/routes.rb
@@ -0,0 +1,19 @@
+Rails.application.routes.draw do
+
+  get 'backoffice', to: 'backoffice/dashboard#index'
+
+  namespace :backoffice do
+    resources :categories, except: [:show, :destroy]
+    get 'dashboard', to: 'dashboard#index'
+  end
+
+  namespace :site do
+    get 'home', to: 'home#index'
+  end
+
+  devise_for :admins
+  devise_for :members
+
+  root 'site/home#index'
+  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+end
